eb99e521dcef6e5a70b85443eb4cdaccac9416c3
Eunji <hyni03@naver.com>
2025-04-02 17:28:10 +0900

    Homework 4

---
+++ main/admin.py
@@ -0,0 +1,8 @@
+# main/admin.py
+from django.contrib import admin
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'created_at')
+    search_fields = ('title', 'content', 'author__username')
